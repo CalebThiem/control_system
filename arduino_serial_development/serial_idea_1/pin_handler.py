83dd437780d6e62468e37b5d91af1452167fa641
Caleb Thiem <calebthiem@protonmail.com>
2023-11-13 10:50:10 +0200

    Continued work on pin_handler.py

--- arduino_serial_development/serial_idea_1/pin_handler.py
+++ arduino_serial_development/serial_idea_1/pin_handler.py
@@ -4,35 +4,48 @@ class PinHandler:
 
         self.pin_array = []
 
+        self.excluded_list = []
+
         for _ in range(48 * 2):
 
             self.pin_array.append("0")
 
-
-    def setLeachRelays(self, relayNumbers):
-
-        for i in range(32):
-
-            self.pin_array[i] = 0
-
-        for j in relayNumbers:
-
-            self.pin_array[j - 1] = 1
-
-    def setElectrowinningRelays(self, relayNumbers):
-
-        for i in range(48, 80):
-
-            self.pin_array[i] = 0
-
-        for j in relayNumbers:
-
-            self.pin_array[j - 1] = 1
-
-    def setRelays(self, relayNumbers):
+    def setRelaysOn(self, relayNumbers):
 
         for i in relayNumbers:
 
-            self.pin_array[i - 1] = 1
+            self.pin_array[i - 1] = "1"
 
-    
+    def setRelaysOff(self, relayNumbers):
+
+        for i in relayNumbers:
+
+            self.pin_array[i - 1] = "0"
+
+    def excludePins(self, relayNumbers):
+
+        for i in relayNumbers:
+
+            if i not in self.excluded_list:
+
+                self.excluded_list.append(i)
+
+    def undoExcludePins(self, relayNumbers):
+
+        for i in relayNumbers:
+
+            if i in self.excluded_list:
+
+                self.excluded_list.remove(i)
+
+    def setLeachRelays(self, relayNumbers):
+
+        for i in range(48):
+
+            self.pin_array[i] = "0"
+
+        for i in relayNumbers:
+
+            if i not in self.excluded_list:
+
+                self.pin_array[i - 1] = "1"
