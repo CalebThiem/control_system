e6e082a3b01361140888c5595cbf013f223831df
Caleb Thiem <calebthiem@protonmail.com>
2023-11-13 15:55:34 +0200

    Finished pinHandler

--- arduino_serial_development/serial_idea_1/pin_handler.py
+++ arduino_serial_development/serial_idea_1/pin_handler.py
@@ -10,18 +10,6 @@ class PinHandler:
 
             self.pin_array.append("0")
 
-    def setRelaysOn(self, relayNumbers):
-
-        for i in relayNumbers:
-
-            self.pin_array[i - 1] = "1"
-
-    def setRelaysOff(self, relayNumbers):
-
-        for i in relayNumbers:
-
-            self.pin_array[i - 1] = "0"
-
     def excludePins(self, relayNumbers):
 
         for i in relayNumbers:
@@ -40,12 +28,42 @@ class PinHandler:
 
     def setLeachRelays(self, relayNumbers):
 
-        for i in range(48):
+        for i in range(49):
 
-            self.pin_array[i] = "0"
+            if i not in self.excluded_list:
+
+                self.pin_array[i - 1] = "0"
 
         for i in relayNumbers:
 
             if i not in self.excluded_list:
 
                 self.pin_array[i - 1] = "1"
+
+    def setElectrowinningRelays(self, relayNumbers):
+
+       for i in range(49, 97):
+
+           if i not in self.excluded_list:
+
+               self.pin_array[i - 1] = "0"
+
+       for i in relayNumbers:
+
+           if i not in self.excluded_list:
+
+               self.pin_array[i - 1] = "1"  
+
+    def setPinsOn(self, relayNumbers):
+
+        for i in relayNumbers:
+
+            self.pin_array[i - 1] = "1"
+
+    def setPinsOff(self, relayNumbers):
+
+        for i in relayNumbers:
+
+            self.pin_array[i - 1] = "0"
+
+
