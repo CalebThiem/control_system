b1f4d55119e4fb3248c5b58a1fcde757347842fb
CalebThiem <calebthiem@protonmail.com>
2024-05-30 12:13:05 +0300

    changed python3-pyserial to python3-serial

--- control_program/code/install.sh
+++ control_program/code/install.sh
@@ -2,7 +2,7 @@ sudo apt install python3-tk -y
 
 sudo apt install python3-pip -y
 
-sudo apt install python3-pyserial -y
+sudo apt install python3-serial -y
 
 sudo apt install python3-matplotlib -y
 
