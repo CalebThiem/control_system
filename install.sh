5d033136907b0bd4d71fcef88dcb6cfd9f35304e
CalebThiem <calebthiem@protonmail.com>
2024-06-13 16:29:36 +0300

    added .desktop file

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ sudo apt install python3-serial -y
 
 sudo apt install python3-matplotlib -y
 
+cp run.desktop /home/user/.local/share/applications
+
 sudo usermod -a -G dialout $USER
 
 sudo shutdown -r now
