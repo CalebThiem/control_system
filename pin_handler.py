4f8a0356e407fe992845cabb58617aa5c19a1df8
CalebThiem <calebthiem@protonmail.com>
2024-06-23 13:58:21 +0300

    added docstring to pin_handler.py

--- pin_handler.py
+++ pin_handler.py
@@ -3,6 +3,60 @@ import threading
 
 class PinHandler:
 
+    '''
+    
+    Manages a list representing activated and deactivated relays. This list is sent to the 
+    Arduino, which activates or deactivates each relay represented in the list. For details, 
+    on how the Arduino receives and interprets the string, see Arduino.ino docstring. 
+
+    Attributes:
+
+        pin_array : list
+            
+            A list representing 96 relays
+
+        excluded_list : list
+
+            A list of relays that will be unaffercted by calls to setLeachRelays, 
+            setElectrowinningRelays, and resetAll.
+
+    Methods: 
+
+        exludePins(list)
+
+            Adds numbers in list parameter to excluded_list
+
+        undoExcludePins(list)
+
+            Removes numbers in list parameter from excluded_list
+
+        setLeachRelays(list)
+
+            For each number in list parameter, sets corresponding entry in pin_array[0:49]
+            to 1, unless in excluded_list.
+
+        setElectrowinnigRelays(list)
+
+            For each number in list parameter, sets corresponding entry in pin_array[49:97] 
+            to 1, unless in excluded_list.
+
+        setRelayOn(list)
+
+            For each number in list parameter, sets corresponding entry in pin_array to 1,
+            across the entirety of pin_array.
+
+        setRelaysOff(list)
+
+            For each number in list parameter, sets corresponding entry in pin_array to 0,
+            across the entirety of pin_array.
+
+        resetAll()
+
+            Sets entries in pin_array to 0, except for entries in excluded_list.
+
+            
+    '''
+
     def __init__(self):
 
         self.pin_array = []
